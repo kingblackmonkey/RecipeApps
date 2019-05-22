89e7ff4e4e32112299107c4c39ccfc08243b78f8
kingblackmonkey <at5724@gmail.com>
2019-05-22 15:14:38 -0700

    finished pagination feature for search results

--- src/js/Controller/index.js
+++ src/js/Controller/index.js
@@ -2,7 +2,7 @@
 
 import Search from '../Model/search'
 import {elements} from  '.././base'
-import * as SearchView from '.././View/SearchView'
+import * as searchView from '.././View/SearchView'
 
 // data storage
 let data = {};
@@ -12,27 +12,35 @@ window.data = data;
 
 // ============== Search Controller=============================
 //event handler for search button
-let  search = async ()=>{
+let  search = async (evt)=>{
+// prevent button default from submitting and reloading the page in the form
+event.preventDefault();
+
      //get query from user 
-  let query = SearchView.getQuery();
+  let query = searchView.getQuery();
  
   if(query){
       //go get result based on provided query
        data.searchResult = new Search(query)
        await data.searchResult.getSearchResult();
-       //display resultof search query
-       SearchView.displayResult(data.searchResult.results)
+       //display result of search query
+       searchView.displayResult(data.searchResult.results)
+      //display pagination button for default page 1
+       searchView.renderPaginationButtons(); 
+      
+       // ======================= pagination buttons Controller using jquery because precoded code was written in jquery
+     //event handler delegation for buttons
+       $(elements.resultContainer).on('click','.cdp_i', function(){
+          //navigate pages when user click on pagination buttons
+          let actPage = searchView.navigatePages($(this),data.searchResult.results.length);
+          //render results according to page
+          searchView.displayResult(data.searchResult.results, actPage)
 
-
-       
+       })
   }
   
 }
-
-
 elements.searchBtn.addEventListener('click', search)
-
-
 
 
 
--- src/js/View/SearchView.js
+++ src/js/View/SearchView.js
@@ -2,7 +2,7 @@ import {elements} from '../base'
 
 
 export let getQuery = ()=>{
-    let query = 'pizza';
+    let query = elements.query.value;
     return query;
 }
 
@@ -22,7 +22,15 @@ let makeHtmlString = (result)=>{
     return html;
 }
 
-export let displayResult = (results)=>{
+
+// ================Pagination ====================
+export let displayResult = (results, page = 1, resPerPage= 10)=>{
+    elements.resultList.innerHTML = '';
+    let startNumber = (page - 1) * resPerPage;
+    let endNum = page * resPerPage
+    // extract result into 10 results per page
+    results  = results.slice(startNumber, endNum);
+
    let html = '';
     results.forEach(result => {
        html =  makeHtmlString(result);
@@ -30,3 +38,49 @@ export let displayResult = (results)=>{
     });
     
 }
+
+// render default pagination
+export let renderPaginationButtons = ()=>{
+    //remove exitsing pagination buttons before render the new one
+    //if pagination buttons exist then remove
+    let paginationContainer = document.querySelector('.cdp');
+
+    if(paginationContainer) elements.resultContainer.removeChild(paginationContainer);
+    
+
+   let html = ` <div class="content_detail__pagination cdp" actpage="1">
+                        <a data-page="#!-1" class="cdp_i">prev</a>
+                        <a data-page="#!1" class="cdp_i">1</a>
+                        <a data-page="#!2" class="cdp_i">2</a>
+                        <a data-page="#!3" class="cdp_i">3</a>			
+                        <a data-page="#!+1" class="cdp_i">next</a>
+                </div>
+              `
+        elements.resultContainer.insertAdjacentHTML('beforeend', html)      
+
+}
+
+
+
+// to  navigate to pages
+export let navigatePages = (button, resultLength)=>{
+   let totalOfPages = Math.ceil(resultLength / 10) ;
+    let actPage = parseInt($('.cdp').attr('actpage'), 10);
+    let dataPage = button.attr('data-page').replace('#!', '');
+            if (dataPage === '+1' && actPage < totalOfPages ) {
+              actPage++;
+            } else if (dataPage === '-1' && actPage > 1 ) {
+              actPage--;
+             
+            }       
+            else{
+               if(button.html() !== 'next' && button.html() !== 'prev' )
+              actPage = parseInt(dataPage, 10);
+            }
+      
+            
+              $('.cdp').attr('actpage', actPage);    
+              return actPage;     
+          
+}
+
