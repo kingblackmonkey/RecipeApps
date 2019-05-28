56fdf13ba47ce7f2f5a9d434da5fd716eabe8037
kingblackmonkey <at5724@gmail.com>
2019-05-27 20:09:40 -0700

    finished individual recipe search feature and update serving feature

--- src/js/Model/search.js
+++ src/js/Model/search.js
@@ -6,11 +6,13 @@ export default class Search{
     }
 
     async getSearchResult(){
+       
         let key = 'c91c51ac267d0b8413dba35491bc1e98';
-        let result = await axios(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
-        
-        this.results = result.data.recipes
-
+        let result = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
+        // console.log(result)
+        this.results = result.data.recipes     
+  
+       
     }
 
 
--- src/js/base.js
+++ src/js/base.js
@@ -3,5 +3,6 @@ export let elements ={
     query:  document.querySelector('.query'),
     resultContainer: document.querySelector('.result-container'),
     resultList: document.querySelector('.result-list'),
-    
+    recipeResult: document.querySelector('.recipe-result'),
+    recipeContainer:document.querySelector('.recipe-container'),
 }
